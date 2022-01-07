08e2a70e8d7604e66826fee8bf6dbf634888bb8e
Louis-yes <luegraham@gmail.com>
2022-01-07 21:19:02 +1100

    - Shifted build logic into dev.horse - This repo will only host site now

--- components/Scripts.js
+++
@@ -1,5 +0,0 @@
-export default function Scripts(site){
-    return `
-        <script src="${site.url}/js/scripts.js"></script>
-    `
-}
--- components/Styles.js
+++
@@ -1,9 +0,0 @@
-export default function styles(site){
-    return `
-        <link rel="stylesheet" href="${site.url}/css/normalize.css">
-        <link rel="stylesheet" href="${site.url}/css/style.css">
-        <style>
-
-        </style>
-    `
-}
